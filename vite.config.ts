fd2dc167ebb7418d04c333403035cbe201003903
Augusto <aspastrana990@gmail.com>
2026-01-16 17:14:56 -0300

    Add new remote

--- vite.config.ts
+++ vite.config.ts
@@ -9,13 +9,19 @@ export default defineConfig({
     react(),
     tailwindcss(),
     federation({
-      name: "host-dec",
+      name: "host",
       remotes: {
         remoteApp: "https://remote-atena.vercel.app/assets/remoteEntry.js",
+        // remoteApp: "http://localhost:5001/assets/remoteEntry.js",
         remoteReactStreamlit:
-          "https://boogie-blizzard.vercel.app/assets/remoteEntry.js",
+          "https://blizzard-streamlit.vercel.app/assets/remoteEntry.js",
+        // remoteReactStreamlit: "http://localhost:5002/assets/remoteEntry.js",
+        // "https://boogie-blizzard.vercel.app/assets/remoteEntry.js",
+        remoteInformation:
+          "https://blizzard-admin.vercel.app/assets/remoteEntry.js",
+        // remoteInformation: "http://localhost:5003/assets/remoteEntry.js",
       },
-      shared: ["react", "react-dom", "react-router-dom"], // 👈 CAMBIO
+      shared: ["react", "react-dom", "react-router-dom"],
     }),
   ],
   build: {
